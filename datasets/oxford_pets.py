66776a1a846533820d693da7eb8a68a17a833cbe
Suho_Kang <54833134+SuhoK@users.noreply.github.com>
2025-07-15 14:29:17 +0900

    Update oxford_pets.py

--- datasets/oxford_pets.py
+++ datasets/oxford_pets.py
@@ -11,7 +11,7 @@ from dassl.utils import read_json, write_json, mkdir_if_missing
 @DATASET_REGISTRY.register()
 class OxfordPets(DatasetBase):
 
-    dataset_dir = "oxford_pets"
+    dataset_dir = "."  # 또는 "OxfordPets"
 
     def __init__(self, cfg):
         # print('cfg ', cfg)
