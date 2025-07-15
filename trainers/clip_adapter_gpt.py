9f3549461b125015d0046891f7c8664c0e1f26fe
Suho_Kang <54833134+SuhoK@users.noreply.github.com>
2025-07-15 15:56:11 +0900

    Update clip_adapter_gpt.py

--- trainers/clip_adapter_gpt.py
+++ trainers/clip_adapter_gpt.py
@@ -243,6 +243,7 @@ class CustomCLIP(nn.Module):
         self.logit_scale = clip_model.logit_scale
         self.dtype = clip_model.dtype
         self.classnames = classnames
+        model_dim = clip_model.visual.transformer.width
 
         we_adapter = cfg.TRAINER.CLIP_ADAPTER.WORD_ADAPTER_TYPE
         self.ratio = cfg.TRAINER.CLIP_ADAPTER.RATIO
