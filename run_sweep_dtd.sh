4a63d2db51e550a1030d26e3a4717f713c70b22e
Suho_Kang <54833134+SuhoK@users.noreply.github.com>
2025-07-16 15:27:29 +0900

    Update run_sweep_dtd.sh

--- run_sweep_dtd.sh
+++ run_sweep_dtd.sh
@@ -17,52 +17,36 @@ gpu_id=${GPUS[$((job_idx % NUM_GPUS))]}
 declare -A SWEEP_PARAMS
 
 # 1 shot (2 combinations)
-SWEEP_PARAMS[1]="cosine 3 1e-4 0.2 linear
-cosine 5 1e-4 0.2 linear"
+SWEEP_PARAMS[1]="cosine 3 5e-5 0.5 linear_residual
+cosine 5 1e-4 0.3 linear_residual"
+
 
 # 2 shot (4 combinations)
-SWEEP_PARAMS[2]="cosine 3 1e-4 0.2 linear
-cosine 5 1e-4 0.2 linear
-cosine 3 1e-4 0.2 linear_residual
-cosine 5 1e-4 0.2 linear_residual"
+SWEEP_PARAMS[2]="cosine 5 5e-5 0.5 linear_residual
+cosine 7 1e-4 0.4 self_attn
+cosine 5 1e-4 0.6 linear_residual"
+
 
 # 4 shot (8 combinations)
-SWEEP_PARAMS[4]="cosine 5 1e-4 0.2 linear
-cosine 7 1e-4 0.2 linear
-single_step 5 1e-4 0.5 linear
-single_step 7 1e-5 0.5 linear_residual
-cosine 5 1e-5 0.5 linear_residual
-cosine 7 1e-4 0.2 linear_residual
-cosine 7 1e-4 0.5 linear
-single_step 5 1e-4 0.2 linear"
+SWEEP_PARAMS[4]="cosine 7 1e-4 0.5 linear_residual
+cosine 10 1e-4 0.6 self_attn
+cosine 7 5e-5 0.4 linear_residual
+cosine 7 1e-4 0.7 self_attn"
+
 
 # 8 shot (8 combinations)
-SWEEP_PARAMS[8]="cosine 7 1e-4 0.2 linear_residual
-single_step 7 1e-4 0.5 linear_residual
-cosine 10 1e-5 0.5 self_attn
-single_step 10 1e-5 0.2 self_attn
-cosine 10 1e-4 0.5 linear_residual
-cosine 7 1e-4 0.2 self_attn
-single_step 7 1e-5 0.2 linear_residual
-cosine 10 1e-5 0.2 linear_residual"
+SWEEP_PARAMS[8]="cosine 7 1e-4 0.5 linear_residual
+cosine 10 1e-4 0.6 linear_residual
+cosine 10 5e-5 0.7 self_attn
+cosine 7 1e-4 0.6 self_attn"
 
 # 16 shot (16 combinations)
-SWEEP_PARAMS[16]="cosine 10 1e-4 0.2 linear_residual
-cosine 15 1e-4 0.2 self_attn
-single_step 10 1e-5 0.5 linear_residual
-cosine 15 1e-5 0.5 linear_residual
-single_step 10 1e-4 0.5 self_attn
-cosine 15 1e-4 0.2 linear_residual
-cosine 10 1e-5 0.2 self_attn
-single_step 15 1e-5 0.5 self_attn
-cosine 15 1e-5 0.2 linear_residual
-single_step 15 1e-5 0.5 linear_residual
-cosine 15 1e-4 0.5 linear_residual
-cosine 10 1e-4 0.2 linear_residual
-single_step 15 1e-4 0.2 self_attn
-cosine 10 1e-4 0.5 self_attn
-single_step 15 1e-4 0.5 linear_residual
-cosine 15 1e-4 0.5 self_attn"
+SWEEP_PARAMS[16]="cosine 10 1e-4 0.5 linear_residual
+cosine 15 1e-4 0.6 self_attn
+cosine 15 5e-5 0.7 linear_residual
+cosine 12 1e-4 0.6 self_attn
+cosine 10 1e-4 0.4 linear_residual"
+
 
 # === Sweep loop ===
 for SHOTS in 1 2 4 8 16
@@ -84,13 +68,13 @@ do
       --dataset-config-file configs/datasets/${DATASET}.yaml \
       --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
       --output-dir ${OUTDIR} \
-      --opts \
-        OPTIM.LR_SCHEDULER ${scheduler} \
-        OPTIM.WARMUP_EPOCH ${warmup} \
-        OPTIM.WARMUP_CONS_LR ${cons_lr} \
-        TRAINER.CLIP_ADAPTER.RATIO ${ratio} \
-        TRAINER.CLIP_ADAPTER.WORD_ADAPTER_TYPE ${adapter} \
-        DATASET.NUM_SHOTS ${SHOTS} \
+      --optim adam \
+      OPTIM.LR_SCHEDULER ${scheduler} \
+      OPTIM.WARMUP_EPOCH ${warmup} \
+      OPTIM.WARMUP_CONS_LR ${cons_lr} \
+      TRAINER.CLIP_ADAPTER.RATIO ${ratio} \
+      TRAINER.CLIP_ADAPTER.WORD_ADAPTER_TYPE ${adapter} \
+      DATASET.NUM_SHOTS ${SHOTS} \
       > ${OUTDIR}/log.txt 2>&1 &
 
     ((job_idx++))
