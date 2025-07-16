1dfb0ae4c011a3a7e5aaf8f488a784644b6165fa
Suho_Kang <54833134+SuhoK@users.noreply.github.com>
2025-07-16 18:19:08 +0900

    Update oxford_flowers.py

--- datasets/oxford_flowers.py
+++ datasets/oxford_flowers.py
@@ -54,34 +54,34 @@ class OxfordFlowers(DatasetBase):
 
         super().__init__(train_x=train, val=val, test=test)
 
-def read_data(self):
-    def _collate(folder, label_offset=0):
-        items = []
-        lab2cname = read_json(self.lab2cname_file)
-        classnames = sorted(os.listdir(folder))
-        for class_idx, cname in enumerate(classnames):
-            class_folder = os.path.join(folder, cname)
-            if not os.path.isdir(class_folder):
-                continue
-            images = sorted(os.listdir(class_folder))
-            for imname in images:
-                impath = os.path.join(class_folder, imname)
-                if os.path.isfile(impath):
-                    items.append(Datum(impath=impath, label=class_idx + label_offset, classname=cname))
-        return items
-
+    def read_data(self):
+        def _collate(folder, label_offset=0):
+            items = []
+            lab2cname = read_json(self.lab2cname_file)
+            classnames = sorted(os.listdir(folder))
+            for class_idx, cname in enumerate(classnames):
+                class_folder = os.path.join(folder, cname)
+                if not os.path.isdir(class_folder):
+                    continue
+                images = sorted(os.listdir(class_folder))
+                for imname in images:
+                    impath = os.path.join(class_folder, imname)
+                    if os.path.isfile(impath):
+                        items.append(Datum(impath=impath, label=class_idx + label_offset, classname=cname))
+            return items
     
-    trainval_items = _collate(os.path.join(self.dataset_dir, "train"))
+        
+        trainval_items = _collate(os.path.join(self.dataset_dir, "train"))
+        
+        
+        random.seed(42)  # reproducibility
+        random.shuffle(trainval_items)
+        n_total = len(trainval_items)
+        n_val = int(n_total * 0.2)
+        val = trainval_items[:n_val]
+        train = trainval_items[n_val:]
     
+        
+        test = _collate(os.path.join(self.dataset_dir, "test"))
     
-    random.seed(42)  # reproducibility
-    random.shuffle(trainval_items)
-    n_total = len(trainval_items)
-    n_val = int(n_total * 0.2)
-    val = trainval_items[:n_val]
-    train = trainval_items[n_val:]
-
-    
-    test = _collate(os.path.join(self.dataset_dir, "test"))
-
-    return train, val, test
+        return train, val, test
