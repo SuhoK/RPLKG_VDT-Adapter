7bd3e66dd6f4d9a573b9ad4a0570b6fad3640fa7
Suho_Kang <54833134+SuhoK@users.noreply.github.com>
2025-07-17 12:21:41 +0900

    Update oxford_flowers.py

--- datasets/oxford_flowers.py
+++ datasets/oxford_flowers.py
@@ -16,7 +16,9 @@ class OxfordFlowers(DatasetBase):
     def __init__(self, cfg):
         root = os.path.abspath(os.path.expanduser(cfg.DATASET.ROOT))
         self.dataset_dir = root #self.dataset_dir = os.path.join(root, self.dataset_dir)
-        self.image_dir = os.path.join(self.dataset_dir, "jpg")
+        #self.image_dir = os.path.join(self.dataset_dir, "jpg")
+        self.train_dir = os.path.join(self.dataset_dir, "train")
+        self.test_dir = os.path.join(self.dataset_dir, "test")
         self.label_file = os.path.join(self.dataset_dir, "imagelabels.mat")
         self.lab2cname_file = os.path.join(self.dataset_dir, "cat_to_name.json")
         self.split_path = os.path.join(self.dataset_dir, "split_zhou_OxfordFlowers.json")
@@ -67,14 +69,14 @@ class OxfordFlowers(DatasetBase):
         def _collate(indices):
             items = []
             for idx in indices:
-                i = int(idx) #- 1  # MATLAB index
-                imname = f"Image_{i}.jpg" #imname = get_imname(i + 1)
-                impath = os.path.join(self.image_dir, imname)
-                label = int(label_file[i - 1])  # Python index는 0-based
-                cname = lab2cname.get(str(label), "unknown")
+                i = int(idx) - 1  # MATLAB index
+                imname = f"Image_{i+1}.jpg" #imname = get_imname(i + 1)
+                impath = os.path.join(folder, imname)
                 if not os.path.isfile(impath):
-                    print(f"[MISSING FILE] {impath}")
+                    print(f"[Missing file] {impath}")
                     continue
+                label = int(label_file[i])
+                cname = lab2cname.get(str(label), "unknown")
                 items.append(Datum(impath=impath, label=label - 1, classname=cname))
             return items
                 #impath = os.path.join(self.image_dir, imname)
