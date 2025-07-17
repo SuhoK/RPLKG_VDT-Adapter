154a5f97201911c310a45f15adbe2c0f2f822c0a
Suho_Kang <54833134+SuhoK@users.noreply.github.com>
2025-07-17 14:52:28 +0900

    Update oxford_flowers.py

--- datasets/oxford_flowers.py
+++ datasets/oxford_flowers.py
@@ -15,29 +15,17 @@ class OxfordFlowers(DatasetBase):
 
     def __init__(self, cfg):
         root = os.path.abspath(os.path.expanduser(cfg.DATASET.ROOT))
-        self.dataset_dir = root #self.dataset_dir = os.path.join(root, self.dataset_dir)
-        self.image_dir = os.path.join(self.dataset_dir, "jpg")
-        self.label_file = os.path.join(self.dataset_dir, "imagelabels.mat")
+        self.dataset_dir = root
+        self.image_dir   = os.path.join(self.dataset_dir, "jpg")
+        self.label_file  = os.path.join(self.dataset_dir, "imagelabels.mat")
+        self.setid_file  = os.path.join(self.dataset_dir, "setid.mat")
         self.lab2cname_file = os.path.join(self.dataset_dir, "cat_to_name.json")
 
-        label_file = loadmat(self.label_file)["labels"][0]
-        
-        lab2cname = read_json(self.lab2cname_file)
-        classnames = [lab2cname[str(i+1)] for i in range(len(lab2cname))]
-        self._classnames = classnames
-
-        self.split_path = os.path.join(self.dataset_dir, "split_zhou_OxfordFlowers.json")
         self.split_fewshot_dir = os.path.join(self.dataset_dir, "split_fewshot")
         mkdir_if_missing(self.split_fewshot_dir)
-
-
-        #if os.path.exists(self.split_path):
-        #    train, val, test = OxfordPets.read_split(self.split_path, self.image_dir)
-        #else:
-        #    train, val, test = self.read_data()
-        #    OxfordPets.save_split(train, val, test, self.split_path, self.image_dir)
-        train, val, test = self.read_data()
         
+        train, val, test = self.read_data()
+      
         num_shots = cfg.DATASET.NUM_SHOTS
         if num_shots >= 1:
             seed = cfg.SEED
@@ -60,7 +48,7 @@ class OxfordFlowers(DatasetBase):
         train, val, test = OxfordPets.subsample_classes(train, val, test, subsample=subsample)
 
         super().__init__(train_x=train, val=val, test=test)
-
+    
     def read_data(self):
         #----------------------------#
         label_file = loadmat(self.label_file)["labels"][0]
@@ -88,35 +76,10 @@ class OxfordFlowers(DatasetBase):
                             for i in sorted(lab2cname,        
                                             key=lambda x: int(x))]
 
-
+        print(len(self.classnames))
+        print(self._classnames[:10])
         return train, val, test
 
     @property
     def classnames(self):
         return self._classnames
-
-            #lab2cname = read_json(self.lab2cname_file)
-            #classnames = sorted(os.listdir(folder))
-            #for class_idx, cname in enumerate(classnames):
-            #    class_folder = os.path.join(folder, cname)
-            #    if not os.path.isdir(class_folder):
-            #        continue
-            #    images = sorted(os.listdir(class_folder))
-            #    for imname in images:
-            #        impath = os.path.join(class_folder, imname)
-            #        if os.path.isfile(impath):
-            #            items.append(Datum(impath=impath, label=class_idx + label_offset, classname=cname))
-            #return items
-    
-        
-        #trainval_items = _collate(os.path.join(self.dataset_dir, "train"))
-        
-        #random.seed(42)  # reproducibility
-        #random.shuffle(trainval_items)
-        #n_total = len(trainval_items)
-        #n_val = int(n_total * 0.2)
-        #val = trainval_items[:n_val]
-        #train = trainval_items[n_val:]
-        #test = _collate(os.path.join(self.dataset_dir, "test"))
-    
-        #return train, val, test
