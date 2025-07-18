93c025fb4935c848e357c53390be71cc41648631
Suho_Kang <54833134+SuhoK@users.noreply.github.com>
2025-07-18 12:51:52 +0900

    Update caltech101.py

--- datasets/caltech101.py
+++ datasets/caltech101.py
@@ -19,11 +19,12 @@ NEW_CNAMES = {
 @DATASET_REGISTRY.register()
 class Caltech101(DatasetBase):
 
-    dataset_dir = "caltech-101"
+    #dataset_dir = "caltech-101"
 
     def __init__(self, cfg):
         root = os.path.abspath(os.path.expanduser(cfg.DATASET.ROOT))
-        self.dataset_dir = os.path.join(root, self.dataset_dir)
+        #self.dataset_dir = os.path.join(root, self.dataset_dir)
+        self.dataset_dir = root
         self.image_dir = os.path.join(self.dataset_dir, "101_ObjectCategories")
         self.split_path = os.path.join(self.dataset_dir, "split_zhou_Caltech101.json")
         self.split_fewshot_dir = os.path.join(self.dataset_dir, "split_fewshot")
