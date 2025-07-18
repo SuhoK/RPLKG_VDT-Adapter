b790fc21790cae1adb937b4d1ddff595b003c9b3
Suho_Kang <54833134+SuhoK@users.noreply.github.com>
2025-07-18 10:32:35 +0900

    Update run_sweep_eurosat.sh

--- run_sweep_EuroSAT.sh
+++ run_sweep_EuroSAT.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # === Editable Configurations ===
-DATA="/home/shkang/VDT_Project/data/flowers102"
+DATA="/home/shkang/VDT_Project/data/eurosat"
 CFG="vit_l14"
 TRAINER="CLIP_Adapter_gpt"
-DATASET="oxford_flowers"
+DATASET="eurosat"
 SEED=1
 TOPK=5
 
